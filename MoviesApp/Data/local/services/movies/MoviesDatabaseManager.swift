1d3fdae7098543b3592158741e1bf7db5c26ec76
Nes <kiquenet85@gmail.com>
2024-10-07 10:39:52 -0500

    Implement Swiftdata to create Movies ans series database manager (#3)

---
+++ MoviesApp/Data/local/services/movies/MoviesDatabaseManager.swift
@@ -0,0 +1,35 @@
+import SwiftData
+import SwiftUI
+
+class MoviesDatabaseManager {
+    let container = try! ModelContainer(for: MovieDB.self)
+
+    @MainActor
+    var modelContext: ModelContext {
+        container.mainContext
+    }
+
+    @MainActor
+    func saveMovies(movies: [MovieDB]) {
+        for movie in movies {
+            modelContext.insert(movie)
+        }
+        do {
+            try modelContext.save()
+        } catch {
+            print("Error saving movies: \(error)")
+        }
+    }
+
+    @MainActor
+    func getMoviesByCategory(category : String) -> [MovieDB]{
+        let moviePredicate = #Predicate<MovieDB> { movie in
+            movie.category == category
+        }
+        let fetchDescriptor = FetchDescriptor<MovieDB>(predicate: moviePredicate,
+                                                     sortBy: [SortDescriptor<MovieDB>(\.title)])
+        let movies = try! modelContext.fetch(fetchDescriptor)
+        return movies
+    }
+
+}
