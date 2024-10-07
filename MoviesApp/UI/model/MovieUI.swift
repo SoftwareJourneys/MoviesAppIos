9a4a78d365ece25dc947f6ec5eb242ba630b33af
nlopezsj <nlopez@softwarejourneys.co>
2024-10-07 08:55:21 -0500

    Add home UI (#4)
    - Add home screen component
    
    - Add mocked data to test
    
    - Add home menu

---
+++ MoviesApp/UI/model/MovieUI.swift
@@ -0,0 +1,17 @@
+//
+//  MovieUI.swift
+//  MoviesApp
+//
+//  Created by Work on 10/7/24.
+//
+
+import Foundation
+
+struct MovieUI: Identifiable, Hashable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var rating: String
+    var date: String
+    var isFavorite: Bool = false
+}
