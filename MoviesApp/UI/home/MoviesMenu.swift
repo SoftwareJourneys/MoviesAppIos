0fb93be51257fe8a475f32c512cbb2d99b0a4205
NONE <NONE>
2024-10-07 08:50:47 -0500

    Add home UI
    - Add home screen component
    
    - Add mocked data to test
    
    - Add home menu

---
+++ MoviesApp/UI/home/MoviesMenu.swift
@@ -0,0 +1,52 @@
+//
+//  MoviesMenu.swift
+//  MoviesApp
+//
+//  Created by Work on 10/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MoviesMenu: View {
+    var menuTitle: String
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text(menuTitle)
+                    .bold()
+                    .font(.title2)
+                Spacer()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 24) {
+                    let movies = previewMokedMovies
+                    ForEach(movies) { movie in
+                        VStack {
+                            AsyncImage(url: URL(string: movie.image)) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 80, height: 100)
+                                    .clipped()
+                            } placeholder: {
+                                ProgressView()
+                                    .frame(width: 80, height: 80)
+                            }
+                            Text(movie.title)
+                                .font(.title2)
+                                .multilineTextAlignment(.center)
+                                .frame(width: 100)
+                                .lineLimit(1)
+                                .truncationMode(.tail)
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+        .frame(alignment: .topLeading)
+        .padding()
+    }
+}
