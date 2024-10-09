e6a0687cadb1a3c617d75a031028c247d0de9654
Nes <kiquenet85@gmail.com>
2024-10-09 15:57:33 -0500

    add tests, atoms and molecules (#28)
    Co-authored-by: Work <work@192.168.1.11>

--- MoviesAppUITests/MoviesAppUITests.swift
+++ MoviesAppUITests/MoviesAppUITests.swift
@@ -6,36 +6,97 @@
 //
 
 import XCTest
+import UIKit
+import SwiftUI
+@testable import MoviesApp
+@testable import MoviesAppTests
 
 final class MoviesAppUITests: XCTestCase {
 
+    var app: XCUIApplication!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        super.setUp()
+        app = XCUIApplication()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    func testContentViewLaunches() throws {
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(app.otherElements["ContentView"].exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testHomeBarNavigationLaunches() throws {
+        app.launch()
+        XCTAssertTrue(app.tabBars.buttons["Home"].exists)
     }
+
+    func testHomeScreenLaunches() throws {
+        app.launch()
+        XCTAssertTrue(app.otherElements["HomeScreen"].exists)
+    }
+
+    func testMoviesMenuLaunches() throws {
+        app.launch()
+        XCTAssertTrue(app.scrollViews["MoviesMenu"].exists)
+    }
+
+    func testPopularMediaSponsorLaunches() throws{
+        app.launch()
+        XCTAssertTrue(app.staticTexts["TV Shows"].exists)
+    }
+
+    func testMovieViewDisplaysCorrectly() throws{
+        app.launch()
+        app.tabBars.buttons["Home"].tap()
+        XCTAssertTrue(app.scrollViews.children(matching: .any).element(boundBy: 0).exists)
+    }
+
+    func testPopularMediaSponsorLoadsImage() throws{
+        app.launch()
+        app.tabBars.buttons["Home"].tap()
+        let movieImage = app.images["NetflixLogo"]
+        XCTAssertTrue(movieImage.exists)
+    }
+
+    func testPlayButtonExists() throws {
+        app.launch()
+        let playButton = app.buttons["Play"]
+        XCTAssertTrue(playButton.exists, "The 'Play' button is not being displayed.")
+    }
+
+    func testPlayButtonInteraction() throws {
+        app.launch()
+        let playButton = app.buttons["Play"]
+        playButton.tap()
+        let alertButton = app.buttons["PlayAlertButton"]
+        let exists = NSPredicate(format: "exists == true")
+        expectation(for: exists, evaluatedWith: alertButton, handler: nil)
+        waitForExpectations(timeout: 2, handler: nil)
+        XCTAssertTrue(alertButton.exists, "The alert is not displayed after pressing 'Play'.")
+    }
+
+    func testMovieTitleExists() throws {
+        app.launch()
+        let movieTitle = app.staticTexts["SponsorMovieTitle"]
+        XCTAssertTrue(movieTitle.exists, "The movie title is not being displayed.")
+    }
+    
+    func testTVShowsLabelExists() throws {
+        app.launch()
+        let tvShowsLabel = app.staticTexts["TV Shows"]
+        XCTAssertTrue(tvShowsLabel.exists, "The text 'TV Shows' is not being displayed.")
+    }
+    
+    func testNetflixLogoExists() throws {
+        app.launch()
+        let netflixLogo = app.images["NetflixLogo"]
+        XCTAssertTrue(netflixLogo.exists, "The Netflix logo is not being displayed.")
+    }
+    
 }
